953c6b7f67604dd3fb28727d22a33707638f0822
tusharhero <tusharmaharana44676@gmail.com>
2022-09-23 17:15:17 +0530

    added two function to correct the cartesian

--- asciiplotter.py
+++ asciiplotter.py
@@ -13,3 +13,37 @@ def plot(equation, img=asc.createblankimg(100, 100), white="⬜", black="⬛"):
             else:
                 img[y][x] = black
     return img
+
+
+def givecartesianpoint(x, y, img):
+    length_y = len(img[0][0])
+    length_x = len(img[0])
+
+    if y == length_y / 2:
+        y = 0
+    else:
+        y = length_y / 2 - y
+
+    if x == length_x / 2:
+        x = 0
+    else:
+        x = length_x / 2 - x
+    return x, y
+
+
+def getcorrectedcoor(x, y, img):
+    length_y = len(img[0][0])
+    length_x = len(img[0])
+    if y == 0:
+        y = length_y / 2
+    if x == 0:
+        x = length_x / 2
+    if y > 0:
+        y = length_y / 2 + y
+    if x > 0:
+        x = length_x / 2 + x
+    if y > 0:
+        y = length_y / 2 - y
+    if x > 0:
+        x = length_x / 2 - x
+    return x, y
