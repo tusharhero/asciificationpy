12ff4505c2a1aade2fe920bc0e9c7f1d9f644a1f
tusharhero <tusharmaharana44676@gmail.com>
2022-09-23 17:46:09 +0530

    correction to func

--- asciiplotter.py
+++ asciiplotter.py
@@ -7,7 +7,9 @@ def plot(equation, img=asc.createblankimg(100, 100), white="⬜", black="⬛"):
         for x in range(
             len(img[y])
         ):  # iterate through each abcissa in the x-axis of the current y-axis
+            # x , y = getcorrectedcoor(x, y, img)
             condition = eval(equation)
+            cx , cy = givecartesianpoint(x, y, img)
             if condition:
                 img[y][x] = white
             else:
@@ -19,15 +21,15 @@ def givecartesianpoint(x, y, img):
     length_y = len(img[0][0])
     length_x = len(img[0])
 
-    if y == length_y / 2:
+    if y == length_y // 2:
         y = 0
     else:
-        y = length_y / 2 - y
+        y = length_y // 2 - y
 
-    if x == length_x / 2:
+    if x == length_x // 2:
         x = 0
     else:
-        x = length_x / 2 - x
+        x = length_x // 2 - x
     return x, y
 
 
@@ -35,15 +37,15 @@ def getcorrectedcoor(x, y, img):
     length_y = len(img[0][0])
     length_x = len(img[0])
     if y == 0:
-        y = length_y / 2
+        y = length_y // 2
     if x == 0:
-        x = length_x / 2
+        x = length_x // 2
     if y > 0:
-        y = length_y / 2 + y
+        y = length_y // 2 + y
     if x > 0:
-        x = length_x / 2 + x
+        x = length_x // 2 + x
     if y > 0:
-        y = length_y / 2 - y
+        y = length_y // 2 - y
     if x > 0:
-        x = length_x / 2 - x
+        x = length_x // 2 - x
     return x, y
