f000536a12bd9af63f4f8ee9268b5014d873fd88
tusharhero <tusharmaharana44676@gmail.com>
2022-09-25 14:01:36 +0530

    added size choosing

--- index.py
+++ index.py
@@ -1,17 +1,22 @@
 import asciirenderer as asc
 import asciiplotter as ascp
-from browser import document , html
+from browser import document, html
 
 
 def getequation(evt):
     equation = document["text-src"].value
-    graph = asc.getstring(ascp.plot(equation, img = asc.createblankimg(50, 50)))
+    size = int(document["size"].value)
+    graph = asc.getstring(ascp.plot(equation, img=asc.createblankimg(size, size)))
     print(graph)
+    document["output"].rows = size
+    document["output"].cols = 2 * size
     document["output"].value = graph
+
 
 def clear(evt):
     document["output"].value = ""
 
+
 document["submit"].bind("click", getequation)
 
-document["clear"].bind("click", clear)  
+document["clear"].bind("click", clear)
