d0890eb65ca79b8acc10e21d394ea55d36936463
tusharhero <tusharmaharana44676@gmail.com>
2022-09-27 19:01:26 +0530

    done ! completely

--- index.py
+++ index.py
@@ -2,28 +2,35 @@ import asciirenderer as asc
 import asciiplotter as ascp
 from browser import document, html
 
-
 def getequation(evt):
     equation = document["text-src"].value
-    size = int(document["size"].value)
-    graph = asc.getstring(ascp.plot(equation, img=asc.createblankimg(size, size)))
-    print(graph)
-    document["box"].width = size
-    document["output"].value = graph
+    try:
+        size = int(document["size"].value)
+        graph = asc.getstring(ascp.plot(equation, img=asc.createblankimg(size, size)))
+        start = time()
+        document["error"].html = "$ checking"
+        document["box"].width = size
+        document["output"].value = graph
+        document["error"].html = "$ done!"
+    except Exception as e:
+        document["error"].html = "$ " + str(e)
 
 def clear(evt):
     document["output"].value = ""
+    document["error"].html = "$ I ate the graph"
 
 def startup():
     eqt = document["-eqt"].value
     size = document["-size"].value
 
-    if eqt == 'default' or size == 'default' : return
+    if eqt == 'default' or size == 'default':
+        return None
 
     i = int(size)
     graph = asc.getstring(ascp.plot(eqt, img=asc.createblankimg(i, i)))
     document["box"].width = i
     document["output"].value = graph
+    document["error"].html = "$ I am done plotting the equation shared to you!"
 
 document["submit"].bind("click", getequation)
 
