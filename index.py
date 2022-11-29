5a27597da62d8671730eb2958dae54c4b29e49b8
BIOLOGY SCIENCE <shiva.science10@gmail.com>
2022-11-29 20:01:09 +0530

    max size limit

--- index.py
+++ index.py
@@ -2,38 +2,66 @@ import asciirenderer as asc
 import asciiplotter as ascp
 from browser import document, html
 print("Hello! https://github.com/tusharhero")
-def getequation(evt):
-    equation = document["text-src"].value
-    try:
-        size = int(document["size"].value)
-        scaling = float(document["scaling"].value)
-        graph = asc.getstring(ascp.plot(equation, img=asc.createblankimg(size, size), s=scaling))
-        document["error"].html = "$ checking"
-        document["box"].width = size
-        document["output"].value = graph
-        document["error"].html = "$ done!"
-    except Exception as e:
-        document["error"].html = "$ " + str(e)
 
 def clear(evt):
     document["output"].value = ""
     document["error"].html = "$ I ate the graph"
 
-def startup():
-    eqt = document["-eqt"].value
-    size = document["-size"].value
-    scaling = document["-scale"].value
-
-    if eqt == 'default' or size == 'default' or scaling == 'default':
-        return None
+def check():
+    eqt = document["text-src"].value
+    size = document["size"].value
+    scaling = document["scaling"].value
 
     i = int(size)
     scale = int(scaling)
 
-    graph = asc.getstring(ascp.plot(eqt, img=asc.createblankimg(i, i), s=scale))
-    document["box"].width = i
-    document["output"].value = graph
-    document["error"].html = "$ I am done plotting the equation shared to you!"
+    if (i > 250):
+        return False
+    else:
+        return [eqt, i, scale]
+
+def getequation(evt):
+    # equation = document["text-src"].value
+    # try:
+    #     size = int(document["size"].value)
+    #     scaling = float(document["scaling"].value)
+    #     graph = asc.getstring(ascp.plot(equation, img=asc.createblankimg(size, size), s=scaling))
+    #     document["error"].html = "$ checking"
+    #     document["box"].width = size
+    #     document["output"].value = graph
+    #     document["error"].html = "$ done!"
+    # except Exception as e:
+    #     document["error"].html = "$ " + str(e)
+    checkData = check()
+
+    if (checkData != False):
+        eqt = checkData[0]
+        size = checkData[1]
+        scale = checkData[2]
+
+        graph = asc.getstring(ascp.plot(eqt, img=asc.createblankimg(size, size), s=scale))
+        document["error"].html = "$ checking"
+        document["box"].width = size
+        document["output"].value = graph
+        document["error"].html = "$ done!"
+
+
+def startup():
+
+    if document["-eqt"] == 'default' or document["-size"] == 'default' or document["-scale"] == 'default':
+        return None
+
+    checkData = check()
+
+    if (checkData != False):
+        eqt = checkData[0]
+        size = checkData[1]
+        scale = checkData[2]
+
+        graph = asc.getstring(ascp.plot(eqt, img=asc.createblankimg(size, size), s=scale))
+        document["box"].width = size
+        document["output"].value = graph
+        document["error"].html = "$ I am done plotting the equation shared to you!"
 
 document["submit"].bind("click", getequation)
 
